1e8c7a97964d3dff0e95cca12b1480dce22d657a
yaroslav <stulnikov.yaroslav@ya.ru>
2025-12-12 22:52:31 +0300

    add: start, jenkiinsfile, fix url

--- start.sh
+++ start.sh
@@ -2,3 +2,8 @@
 docker compose -f ./_devops/nexus/docker-compose.yaml up -d
 docker compose -f ./_devops/jenkins/docker-compose.yaml up -d
 minikube start
+
+#helm repo add prometheus-community https://prometheus-community.github.io/helm-charts 
+#helm install monitoring prometheus-community/kube-prometheus-stack -n monitoring --create-namespace
+#helm install blackbox-exporter prometheus-community/prometheus-blackbox-exporter -n monitoring
+#helm upgrade monitoring prometheus-community/kube-prometheus-stack -n monitoring -f values.yaml
